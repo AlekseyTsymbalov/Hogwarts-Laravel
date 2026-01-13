57fb88984a38f518738a705ac5480aaf88dd987d
Guyver <AlekseyTsymbalov@yahoo.com>
2026-01-13 23:07:06 +0300

    пробросил маршрут для авторизации. Создал Route::get и Route::post, в web.php.

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'show']);
+Route::post('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
