5a79ffe84a860e7f1a635b2032f2dfd513c15694
Guyver <AlekseyTsymbalov@yahoo.com>
2026-01-13 23:37:59 +0300

    Создал контроллер для регистрации нового пользователя.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,20 +1,22 @@
 <?php
 
-declare(strict_types = 1);
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Validation;
-use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
 
 class RegisterController extends Controller
 {
-    public function register(Request $request)
+    public function show(): View
     {
+        return view('auth.register');
+    }
 
+    public function store(Request $request)
+    {
+        //email
+        //password
+        //name
     }
 }
