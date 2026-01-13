01e1c4f76d24cd84dae1cd9733485ca2b35f3dd0
Guyver <AlekseyTsymbalov@yahoo.com>
2026-01-14 00:04:13 +0300

    Создал простую форму регистрации. Дополинл роуты для регистрации, провёл форматирование кода по PSR-12

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'show']);
-Route::post('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
+Route::get(
+    '/register',
+    [RegisterController::class, 'show']
+)->name('register.show');
+Route::post(
+    '/register',
+    [RegisterController::class, 'store']
+)->name('register.store');
