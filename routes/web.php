ff4372d2fdab567b2a7492b15b7e6b1703f09990
Guyver <AlekseyTsymbalov@yahoo.com>
2026-01-20 17:10:06 +0300

    Убрал все маршруты web.php

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,3 @@
 
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
-
-Route::get(
-    '/register',
-    [RegisterController::class, 'show']
-)->name('register.show');
-Route::post(
-    '/register',
-    [RegisterController::class, 'store']
-)->name('register.store');
