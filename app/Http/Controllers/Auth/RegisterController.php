489f610e72c590e4fbc6339b9dbf75d84cc25c21
Guyver <AlekseyTsymbalov@yahoo.com>
2026-01-20 18:06:57 +0300

    Рефакторинг кода api.php, переписал в чистый API RegisterController.php. Создал RegisterRequest.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,43 +2,19 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
-//    public function show(): View
-//    {
-//        return view('auth.register');
-//    }
-
-    public function register(Request $request): JsonResponse
+    public function register(RegisterRequest $request)
     {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'phone' => ['required', 'string', 'max:20'],
-            'password' => ['required', 'string', 'min:8', 'confirmed']
-        ]);
+        $user = User::create($request->validated());
 
-        $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'phone' => $validated['phone'],
-            'password' => $validated['password'],
-        ]);
-
-        return response()->json([
-            'success' => true,
-            'data' => [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-                'phone' => $user->phone,
-            ],
-            'error' => null,
-        ], 201);
+        return $this->createdResponse(
+            new UserResource($user)
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::post(
-        '/register',
-        [RegisterController::class, 'register']
-    );
-
-    Route::post(
-        '/signin',
-        [UserController::class, 'signin']
-    );
+    Route::post('/register', [RegisterController::class, 'register']);
+    Route::post('/signin', [UserController::class, 'signin']);
 });
-
 
 Route::get('/test-error', [UserController::class, 'testException']);
