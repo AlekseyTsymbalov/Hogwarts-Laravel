3cf48501d30a15f670158e03a50947540710a431
Guyver <AlekseyTsymbalov@yahoo.com>
2026-01-23 11:16:45 +0300

    Рефакторинг для группировки

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,16 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\AuthController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [RegisterController::class, 'register']);
     Route::post('/signin', [AuthController::class, 'signin'])->middleware('throttle:5,1');
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/signout', [AuthController::class, 'signout']);
-    });
 });
+
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/auth/signout', [AuthController::class, 'signout']);
     Route::get('/profile', [ProfileController::class, 'show']);
 });
