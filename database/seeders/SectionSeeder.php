1a7e55b6bd8f9b1d001aa07d7e0c897a42f7a19e
Guyver <AlekseyTsymbalov@yahoo.com>
2026-01-23 19:56:38 +0300

    Добавил use в SectionSeeder.php

--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Section;
 use Illuminate\Database\Seeder;
 
 class SectionSeeder extends Seeder
