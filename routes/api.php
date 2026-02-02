c2eb4ab69ee866f4a1cc911acea036a045891c9d
Guyver <AlekseyTsymbalov@yahoo.com>
2026-02-02 18:46:08 +0300

    Добавил новые роуты для работы с корзиной.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BasketController;
 use App\Http\Controllers\WishlistController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ProductsController;
@@ -18,6 +19,14 @@ Route::prefix('auth')->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [UserController::class, 'profile']);
+
+    Route::prefix('basket')->group(function () {
+        Route::get('/add', [BasketController::class, 'add']);
+        Route::get('/{id}', [BasketController::class, 'update']);
+        Route::get('/{id}', [BasketController::class, 'delete']);
+        Route::get('/', [BasketController::class, 'list']);
+        Route::get('/', [BasketController::class, 'clear']);
+    });
 
     Route::prefix('wishlist')->group(function () {
         Route::post('/add', [WishlistController::class, 'add']);
